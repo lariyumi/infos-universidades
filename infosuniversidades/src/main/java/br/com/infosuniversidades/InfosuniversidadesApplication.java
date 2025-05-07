e75977d67117b0650ec37aa66db4e3eec4ffbc97
Larissa Ohashi <lariyumi92@gmail.com>
2025-05-07 11:31:31 -0300

    Criado repositorio e adicionados arquivos iniciais

---
+++ infosuniversidades/src/main/java/br/com/infosuniversidades/InfosuniversidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.infosuniversidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosuniversidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosuniversidadesApplication.class, args);
+	}
+
+}
---
+++ infosuniversidades/src/test/java/br/com/infosuniversidades/InfosuniversidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infosuniversidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosuniversidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
