20df6a92679ae66455926361ecdb2ce505f2b797
Larissa Ohashi <lariyumi92@gmail.com>
2025-05-12 13:31:44 -0300

    Adicionada funcionalidade de login

--- infosuniversidades/src/main/java/br/com/infosuniversidades/controllers/UsuarioController.java
+++ infosuniversidades/src/main/java/br/com/infosuniversidades/controllers/UsuarioController.java
@@ -1,17 +1,22 @@
 package br.com.infosuniversidades.controllers;
 
 import br.com.infosuniversidades.dto.RequisicaoFormUsuario;
+import br.com.infosuniversidades.dto.RequisicaoLoginUsuario;
 import br.com.infosuniversidades.models.Usuario;
 import br.com.infosuniversidades.repositories.UsuarioRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class UsuarioController {
@@ -20,42 +25,100 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     @GetMapping("/cadastro")
-    public ModelAndView cadastro(RequisicaoFormUsuario requisicao) {
-        ModelAndView mv = new ModelAndView("cadastro");
+    public ModelAndView cadastro(RequisicaoFormUsuario requisicao, HttpServletRequest req) {
+        if (req.getSession(false) == null) {
+            ModelAndView mv = new ModelAndView("cadastro");
 
-        return mv;
+            return mv;
+        } else {
+            ModelAndView mv = new ModelAndView("redirect:/");
+            mv.addObject("mensagem", "Saia da sua conta antes de tentar logar/cadastrar uma nova conta");
+
+            return mv;
+        }
     }
 
     @PostMapping("/cadastro")
     public ModelAndView criar(@Valid RequisicaoFormUsuario requisicao, BindingResult binding, HttpServletRequest req) {
         if (binding.hasErrors()) {
-            return new ModelAndView("/cadastro");
+            return new ModelAndView("cadastro");
         } else {
             //cria um usuário
             Usuario usuario = requisicao.toUsuario();
             if (this.usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
-                ModelAndView mv = new ModelAndView("/cadastro");
+                ModelAndView mv = new ModelAndView("cadastro");
                 mv.addObject("mensagem", "O e-mail já foi utilizado para outro usuário");
 
                 return mv;
             } else {
-                if (req.getSession(false) == null) {
-                    //Salva o usuário no banco de dados
-                    this.usuarioRepository.save(usuario);
+                //Salva o usuário no banco de dados
+                this.usuarioRepository.save(usuario);
 
-                    HttpSession session = req.getSession(true);
-                    session.setAttribute("email", usuario.getEmail());
-                    session.setAttribute("nome", usuario.getNome());
-                    session.setAttribute("role", usuario.getTipo().toString());
+                HttpSession session = req.getSession(true);
+                session.setAttribute("email", usuario.getEmail());
+                session.setAttribute("nome", usuario.getNome());
+                session.setAttribute("role", usuario.getTipo().toString());
 
-                    return new ModelAndView("redirect:/");
-                } else {
-                    ModelAndView mv = new ModelAndView("redirect:/");
-                    mv.addObject("mensagem", "Saia da sua conta antes de tentar logar/cadastrar uma nova conta");
-
-                    return mv;
-                }
+                return new ModelAndView("redirect:/");
             }
+        }
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login(RequisicaoLoginUsuario requisicao, HttpServletRequest req) {
+        if (req.getSession(false) == null) {
+            ModelAndView mv = new ModelAndView("login");
+
+            return mv;
+        } else {
+            ModelAndView mv = new ModelAndView("redirect:/");
+            mv.addObject("mensagem", "Saia da sua conta antes de tentar logar/cadastrar uma nova conta");
+
+            return mv;
+        }
+    }
+
+    @PostMapping("/login")
+    public ModelAndView logar(@Valid RequisicaoLoginUsuario requisicao, BindingResult binding, HttpServletRequest req) {
+        if (binding.hasErrors()) {
+            return new ModelAndView("login");
+        } else {
+            //cria um usuário
+            Optional<Usuario> usuarioLogado = this.usuarioRepository.findByEmail(requisicao.getEmail());
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(16);
+            System.out.println("************************" + usuarioLogado.isPresent());
+            System.out.println(requisicao.getSenha());
+            System.out.println(usuarioLogado.get().getSenha());
+            System.out.println(passwordEncoder.matches(requisicao.getSenha(), usuarioLogado.get().getSenha()));
+
+            if (usuarioLogado.isPresent() && passwordEncoder.matches(requisicao.getSenha(), usuarioLogado.get().getSenha())) {
+                System.out.println("***************Entrou aqui");
+                HttpSession session = req.getSession(true);
+                session.setAttribute("email", usuarioLogado.get().getEmail());
+                session.setAttribute("nome", usuarioLogado.get().getNome());
+                session.setAttribute("role", usuarioLogado.get().getTipo().toString());
+
+                return new ModelAndView("redirect:/");
+            } else {
+                ModelAndView mv = new ModelAndView("login");
+                mv.addObject("mensagem", "E-mail ou senha inválidos");
+
+                return mv;
+            }
+        }
+    }
+
+    @GetMapping("/login/alteracao-senha")
+    public ModelAndView alterarSenha(RequisicaoFormUsuario requisicao, HttpServletRequest req) {
+        if (req.getSession(false) == null) {
+            ModelAndView mv = new ModelAndView("alteracao-senha");
+
+            return mv;
+        } else {
+            ModelAndView mv = new ModelAndView("redirect:/");
+            mv.addObject("mensagem", "Saia da sua conta antes de tentar logar/cadastrar uma nova conta");
+
+            return mv;
         }
     }
 
---
+++ infosuniversidades/src/main/java/br/com/infosuniversidades/dto/RequisicaoLoginUsuario.java
@@ -0,0 +1,70 @@
+package br.com.infosuniversidades.dto;
+
+import br.com.infosuniversidades.models.TipoUsuario;
+import br.com.infosuniversidades.models.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class RequisicaoLoginUsuario {
+
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String senha;
+    private TipoUsuario tipoUsuario;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Usuario toUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setEmail(this.email);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String senhaCriptografada = encoder.encode(this.senha);
+        usuario.setSenha(senhaCriptografada);
+        usuario.setTipo(TipoUsuario.COMUM);
+
+        return usuario;
+    }
+
+    public Usuario toUsuario(Usuario usuario) {
+        usuario.setEmail(this.email);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String senhaCriptografada = encoder.encode(this.senha);
+        usuario.setSenha(senhaCriptografada);
+        usuario.setTipo(TipoUsuario.COMUM);
+
+        return usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "RequisicaoFormUsuario{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+
+}
